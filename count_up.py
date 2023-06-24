2fb490c6b10c7857bf5ef8e428db8fa26486f7a7
Ross2010 <108616916+Ross2010@users.noreply.github.com>
2023-06-24 09:10:10 -0400

    Add files via upload

---
+++ count_up.py
@@ -0,0 +1,6 @@
+def count_up(start, stop):
+    nums = list(range(start, stop + 1))
+    for i in nums:
+        print(i)
+
+count_up(5, 7)
---
+++ sum.py
@@ -0,0 +1,7 @@
+def sum_nums(nums):
+    sum = 0
+    for i in nums:
+        sum = sum + i
+    print(sum)
+
+sum_nums([1, 2, 3, 4])
